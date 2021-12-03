922e5ec30b326fc31273cb1b47869c3635baf10a
Gustavo Trindade <gu.trindadelive@gmail.com>
2021-12-03 11:15:50 -0300

    add tsyringe listCategories

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
 
-    constructor(private ListCategoriesUseCase: ListCategoriesUseCase ) {}
 
+    async handle(req: Request, res: Response): Promise<Response> {
 
-    handle(req: Request, res: Response): Response {
-
-        const all = this.ListCategoriesUseCase.execute();
-
+        const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);  
+        
+        const all = await listCategoriesUseCase.execute();
+        
         return res.json(all);
 
     }
