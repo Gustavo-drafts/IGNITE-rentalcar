dc11d6c2f2635dc5f5f0dc5d4bb15dc19060fc13
Gustavo Trindade <gu.trindadelive@gmail.com>
2021-12-06 12:56:57 -0300

    user module

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidV4 } from "uuid";
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 
 @Entity("categories")
@@ -18,7 +18,7 @@ class Category {
 
     constructor() {
         if(!this.id) {
-            this.id = uuidv4();
+            this.id = uuidV4();
         }
     }
 }
